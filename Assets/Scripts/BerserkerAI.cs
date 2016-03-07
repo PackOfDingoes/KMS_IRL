01d33e78840d8835690959c42434b474ff06effe
PackOfDingoes <silverback.jed@gmail.com>
2016-03-07 12:44:26 +1000

    AI changes
    -tweaked sniper AI
    -added berserker AI

---
+++ Assets/Scripts/BerserkerAI.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BerserkerAI : MonoBehaviour
+{
+    public float walkSpeed = 2.5f;
+    public float sprintSpeed = 20f;
+    private float move;
+    private Rigidbody2D m_Rigidbody2D;
+    private bool playerSpotted = false;
+    private bool facingRight = false;
+    public Transform floorCheck;
+    public Transform playerCheck;
+    public Transform wallCheck;
+    public Transform berzerkerPos;
+    // Use this for initialization
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerSpotted == false)
+        {
+            move = walkSpeed;
+        }
+        if (playerSpotted == true)
+        {
+            move = sprintSpeed;
+        }
+    }
+    void FixedUpdate()
+    {
+        RayCasts();
+        Move();
+    }
+
+    public void Move()
+    {
+        if (facingRight == false)
+        {
+            m_Rigidbody2D.velocity = new Vector2(-move, m_Rigidbody2D.velocity.y);
+        }
+        if (facingRight == true)
+        {
+            m_Rigidbody2D.velocity = new Vector2(move, m_Rigidbody2D.velocity.y);
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    void RayCasts()
+    {
+        RaycastHit2D floorIsGone = Physics2D.Raycast(berzerkerPos.position, -berzerkerPos.position + floorCheck.position, Vector2.Distance(berzerkerPos.position, floorCheck.position));
+        RaycastHit2D playerInfront = Physics2D.Raycast(berzerkerPos.position, -berzerkerPos.position + playerCheck.position, Vector2.Distance(berzerkerPos.position, playerCheck.position));
+        RaycastHit2D wallInWay = Physics2D.Raycast(berzerkerPos.position, -berzerkerPos.position + wallCheck.position, Vector2.Distance(berzerkerPos.position, wallCheck.position));
+        /*Debug.DrawLine(berzerkerPos.position, floorIsGone.point, Color.red);
+        Debug.DrawLine(berzerkerPos.position, playerInfront.point, Color.red);
+        Debug.DrawLine(berzerkerPos.position, wallInWay.point, Color.red);*/
+        if (floorIsGone.collider == null)
+        {
+            Flip();
+            Debug.Log("flip");
+        }
+
+        if (wallInWay.collider != null)
+        {
+            if (wallInWay.collider.tag != "Player")
+            {
+                Flip();
+            }
+
+        }
+
+        if (playerInfront.collider != null)
+        {
+            if(playerInfront.collider.tag == "Player")
+            {
+                playerSpotted = true;
+                Debug.Log(playerSpotted);
+            }
+        }
+    }
+
+}
--- Assets/Scripts/SniperAI.cs
+++ Assets/Scripts/SniperAI.cs
@@ -5,30 +5,43 @@ public class SniperAI : MonoBehaviour
 {
     public Transform sniperScope;
     public Transform playerPos;
-    private bool playerInSights = false;
+    private bool shoot = false;
+    public float sniperRange = 15f;
+    //private bool playerInSights = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Debug.Log(shoot);
         RayCast();
     }
 
     void RayCast()
     {
-        playerInSights = Physics2D.Linecast(sniperScope.position, playerPos.position, 1 << LayerMask.NameToLayer("Ground"));
-        if (playerInSights == false)
+        //casts ray from sniperscope to player pos (-sniperScope.position + playerPos.position) = player direction
+        RaycastHit2D playerInSights = Physics2D.Raycast(sniperScope.position, -sniperScope.position + playerPos.position, sniperRange);
+        if (playerInSights.collider !=null )
         {
-            Debug.DrawLine(sniperScope.position, playerPos.position, Color.green);
+            //Debug.DrawLine(sniperScope.position, playerInSights.point, Color.red);
+            if (playerInSights.collider.tag == "Player")
+            {
+                shoot = true;
+            }
+            if (playerInSights.collider.tag != "Player")
+            {
+                shoot = false;
+            }
         }
-        if (playerInSights == true)
+        if (playerInSights.collider == null)
         {
-            Debug.DrawLine(sniperScope.position, playerPos.position, Color.red);
+            shoot = false;
         }
     }
 }
