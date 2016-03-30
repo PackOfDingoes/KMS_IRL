0f24be28f8ea73a3c5d7cfd57f30caa6f5b86b67
Packofdingoes <silverback.jed@gmail.com>
2016-03-30 12:40:24 +1000

    some little scripts

---
+++ Assets/Scripts/MERGE THESE/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float destroyTimer = 0f;
+    public float destroyAfter = 1f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rb2d.velocity.x == 0 && rb2d.velocity.y == 0)
+        {
+            destroyTimer = destroyTimer + Time.deltaTime;
+        }
+        else if (rb2d.velocity.x != 0 && rb2d.velocity.y != 0)
+        {
+            destroyTimer = 0;
+        }
+        if (destroyTimer > destroyAfter)
+        {
+            Destroy(this.gameObject);
+        }
+        Debug.Log(destroyTimer);
+    }
+}
---
+++ Assets/Scripts/MERGE THESE/Boulder.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boulder : MonoBehaviour {
+
+    private int bounceCount = 0;
+    public int bounceMax = 3;
+    public GameObject Nova;
+    private bool makeBox = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(bounceCount);
+	    if (bounceCount > bounceMax)
+        {
+            Destroy(this.gameObject);
+        }
+        if (bounceCount > 0 && makeBox == true)
+        {
+            StartCoroutine(CreateABoxForSomeReason(0.2f));
+            makeBox = false;
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        bounceCount++;
+    }
+
+    IEnumerator CreateABoxForSomeReason(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        GameObject nova = Instantiate(Nova, this.transform.position, Quaternion.identity) as GameObject;
+        nova.transform.parent = gameObject.transform;
+    }
+}
